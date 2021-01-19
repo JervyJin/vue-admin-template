5c660583e035cdda430de59377e10a6f11053da9
jinjianwei <11510706292@qq.com>
2021-01-19 15:02:20 +0800

    i18n更改

--- src/views/products/products.vue
+++ src/views/products/products.vue
@@ -9,7 +9,7 @@
       highlight-current-row
     >
       <el-table-column align="center" label="序号" width="95">
-        <template slot-scope="scope">{{ scope.$index }}</template>
+        <template slot-scope="scope">{{ scope.$index+1 }}</template>
       </el-table-column>
       <el-table-column label="商品标题">
         <template slot-scope="scope">
@@ -17,13 +17,22 @@
         </template>
       </el-table-column>
     </el-table>
+    <!-- 分页组件 -->
+    <pagination
+    :total="total"
+    :page.sync="listQuery.page"
+    :limit.sync="listQuery.limit"
+    @pagination="getList" />
   </div>
 </template>
 
 <script>
 import { getList } from "@/api/table";
+import { fetchList, fetchPv, createArticle, updateArticle } from '@/api/article'
+import Pagination from '@/components/Pagination'
 
 export default {
+  components: { Pagination },
   filters: {
     statusFilter(status) {
       const statusMap = {
@@ -37,20 +46,39 @@ export default {
   data() {
     return {
       list: null,
-      listLoading: true
+      listLoading: true,
+      total: 0,
+      listQuery: {
+        page: 1,
+        limit: 20
+      }
     };
   },
   created() {
     this.fetchData();
+    this.getList();
   },
   methods: {
+    /**获取表格数据*/ 
     fetchData() {
       this.listLoading = true;
       getList().then(response => {
         this.list = response.data.items;
         this.listLoading = false;
       });
-    }
+    },
+    /*分页方法，点击分页是要请求后台数据的，这里的Pagination组件是真分页*/ 
+    getList() {
+      this.listLoading = true
+      fetchList(this.listQuery).then(response => {
+        this.list = response.data.items
+        this.total = response.data.total
+        // Just to simulate the time of the request
+        setTimeout(() => {
+          this.listLoading = false
+        }, 1.5 * 1000)
+      })
+    },
   }
 };
 </script>
